4e7e2ea60f010783fcc3fd0f54a89fe0ab803b8d
Yazky Maulana <yazki.maulana@gmail.com>
2021-11-16 14:27:09 +0700

    done migrations

---
+++ database/migrations/2021_11_16_070432_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+                $table->unsignedBigInteger('users_id');
+                $table->foreign('users_id')->references('id')->on('users');
+                $table->unsignedBigInteger('products_id');
+                $table->foreign('products_id')->references('id')->on('products');
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
